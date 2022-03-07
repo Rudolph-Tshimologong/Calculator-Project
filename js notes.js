e59131f986ec2f92f2b0b71ebf1e21f6881474dd
Rudolph <98885973+Rudolph-Tshimologong@users.noreply.github.com>
2022-03-07 15:36:03 +0200

    Update js notes.js
    concatenating

--- js notes.js
+++ js notes.js
@@ -4,4 +4,6 @@
 		name = "Rudolph";
 		console.log("Hello" + " " + name); 
 		/* OR */  
+		//plus sign is used to concatinate
 		console.log(` Hello ${name} `);
+		//dollar sign and curly brackets to concatinate
